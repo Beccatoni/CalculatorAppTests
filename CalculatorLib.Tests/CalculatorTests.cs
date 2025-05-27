e539c8c67e221c0eb1bea3510ac20879660ef437
Rebecca Mutoni <rbccmtni1gmail.com>
2025-05-27 10:01:10 +0200

    feat: instead of Assert.AreEqual() method which doesn't exist in NUnit use Assert.That method.

---
+++ CalculatorLib.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+namespace CalculatorLib.Tests;
+
+public class Tests
+{
+    private Calculator _calculator;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Test]
+    public void Add_ReturnsSum()
+    {
+        Assert.That(_calculator.Add(5, 5), Is.EqualTo(10));
+        //Assert.AreEqual(10, _calculator.Add(5, 5));
+        //ClassicAssert.AreEqual(10, _calculator.Add(5, 5));
+    }
+}
